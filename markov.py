a0afea801ba08f0f7b8f4055fbc39f862bc94ba1
rdsheppard95 <xxryan9559xx@gmail.com>
2015-08-06 00:38:20 -0400

    Added in a random statesize and logging to a file

--- markov.py
+++ markov.py
@@ -1,5 +1,6 @@
 import markovify
 import tweepy
+import random
 
 # Opens consumer file
 with open('consumerTokens') as f:
@@ -21,11 +22,21 @@ api = tweepy.API(auth)
 with open('txt/picard.txt') as f:
     text = f.read()
 
+# Generate random integer for state size
+random_state_size = random.randrange(2, 4)
+
 # Create text model
-text_model = markovify.Text(text)
+text_model = markovify.Text(text, state_size=random_state_size)
 
 # Create a sentence limited to 140 chars
 output_text = text_model.make_short_sentence(140)
 
 # Post the update to Twitter
 api.update_status(status=output_text)
+
+# Write the status to a file, along with state size
+with open('history.txt', 'a') as f:
+
+    f.write(output_text + '\n')
+    f.write('State size of: ')
+    f.write(str(random_state_size) + '\n\n')
