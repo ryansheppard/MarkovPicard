d0cc5feb7d0e44630d4df2b46a43409e8597071c
rdsheppard95 <xxryan9559xx@gmail.com>
2015-08-06 10:51:48 -0400

    Added in code for a drunk picard

--- markov.py
+++ markov.py
@@ -1,6 +1,8 @@
 import markovify
 import tweepy
 import random
+import datetime
+
 
 # Starts the api and auth
 with open('consumerTokens') as f:
@@ -22,19 +24,29 @@ def generate_post():
     with open('txt/picard.txt') as f:
         text = f.read()
 
-    # Generate random integer for state size
-    random_state_size = random.randrange(2, 4)
+    # Decides if it's late enough for Picard to be drunk
+    t = datetime.datetime.now().time()
+    start_t = datetime.time(0, 0, 0)
+    end_t = datetime.time(4, 0, 0)
+    if start_t <= t <= end_t:
+        gen_state_size = 1
+        text_model = markovify.Text(text, state_size=gen_state_size)
+    else:
+        gen_state_size = random.randrange(2, 4)
+        text_model = markovify.Text(text, state_size=gen_state_size)
 
-    text_model = markovify.Text(text, state_size=random_state_size)
 
     # Create a sentence limited to 140 chars
-    output_text = text_model.make_short_sentence(140)
+    if gen_state_size == 1:
+        output_text = text_model.make_short_sentence(127) + " #DrunkPicard"
+    else:
+        output_text = text_model.make_short_sentence(140)
 
     # Write the status to a file, along with state size
     with open('history.txt', 'a') as f:
         f.write(output_text + '\n')
         f.write('State size of: ')
-        f.write(str(random_state_size) + '\n\n')
+        f.write(str(gen_state_size) + '\n\n')
 
     return output_text
 
