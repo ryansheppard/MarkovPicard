669e17340d2431e8f61f686247a9339f16f7428a
rdsheppard95 <xxryan9559xx@gmail.com>
2015-08-06 01:11:14 -0400

    Cleaned up code

--- markov.py
+++ markov.py
@@ -2,41 +2,42 @@ import markovify
 import tweepy
 import random
 
-# Opens consumer file
+# Starts the api and auth
 with open('consumerTokens') as f:
     consumer_key = f.readline().strip()
     consumer_secret = f.readline().strip()
 
-# Opens access file
 with open('accessTokens') as f:
     access_token = f.readline().strip()
     access_token_secret = f.readline().strip()
 
-# Create auth and api
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-# Read text file
-with open('txt/picard.txt') as f:
-    text = f.read()
 
-# Generate random integer for state size
-random_state_size = random.randrange(2, 4)
+# Creates the post and logs to a file
+def generate_post():
+    with open('txt/picard.txt') as f:
+        text = f.read()
 
-# Create text model
-text_model = markovify.Text(text, state_size=random_state_size)
+    # Generate random integer for state size
+    random_state_size = random.randrange(2, 4)
 
-# Create a sentence limited to 140 chars
-output_text = text_model.make_short_sentence(140)
+    text_model = markovify.Text(text, state_size=random_state_size)
 
-# Post the update to Twitter
-api.update_status(status=output_text)
+    # Create a sentence limited to 140 chars
+    output_text = text_model.make_short_sentence(140)
 
-# Write the status to a file, along with state size
-with open('history.txt', 'a') as f:
+    # Write the status to a file, along with state size
+    with open('history.txt', 'a') as f:
+        f.write(output_text + '\n')
+        f.write('State size of: ')
+        f.write(str(random_state_size) + '\n\n')
 
-    f.write(output_text + '\n')
-    f.write('State size of: ')
-    f.write(str(random_state_size) + '\n\n')
+    return output_text
+
+
+# Post the status to Twitter
+api.update_status(status=generate_post())
